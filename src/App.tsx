2413cb17fc0f2af26ec7ca1440afb89fa4dc1e7d
moon <mjm7042@wavus.co.kr>
2024-03-15 15:24:38 +0900

    ver.1.8

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,40 @@ import PageNavigator from "./PageNavigator";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import ScrollToUp from "./util/ScrollToUp";
+import Swal from "sweetalert2";
 import { AuthProvider } from "./contexts/AuthProvider";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {}
-  }
-});
+import { useMediaQuery } from "react-responsive";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function App() {
+  const isMobile = useMediaQuery({
+    query: "(max-width: 1024px)"
+  });
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isMobile) {
+      Swal.fire({
+        text: "모바일 페이지는 준비중입니다",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "coral",
+        cancelButtonColor: "gray",
+        confirmButtonText: "인스타 계정 이동",
+        cancelButtonText: "확인"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          navigate("https://www.instagram.com/jejumoanistay/");
+        } else {
+          navigate(-1);
+        }
+      });
+    }
+  });
+
   return (
-    <QueryClientProvider client={queryClient}>
+    <>
       <AuthProvider>
         <Header />
         <main>
@@ -22,7 +44,7 @@ function App() {
         <Footer />
       </AuthProvider>
       <ScrollToUp />
-    </QueryClientProvider>
+    </>
   );
 }
 
